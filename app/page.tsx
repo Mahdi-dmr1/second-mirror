c4d485289d44d08a71587333656b6d965af13dd6
Mohammad <damirchi159@gmail.com>
2025-06-08 16:47:43 -0400

    add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 
 const Page = () => {
+    const iframeRef = useRef<HTMLIFrameElement | null>(null);
     useEffect(() => {
         const handleMessage = (event: MessageEvent) => {
             console.log('Message received from iframe:', event.data);
@@ -12,14 +13,22 @@ const Page = () => {
         };
     }, []);
 
-  return(
-      <div className="h-dvh w-full flex justify-center items-center">
-        <div className="w-full bg-gray-500 flex flex-col justify-center h-full items-center">
-            <p className="text-black">This is the second page</p>
-            <iframe className="h-full w-full" src="https://first-mirror.vercel.app/" />
+    const sendMessageToIframe = () => {
+        const iframe = iframeRef.current;
+        if (iframe && iframe.contentWindow) {
+            iframe.contentWindow.postMessage('Hello from parent!', '*');
+        }
+    };
+
+    return(
+        <div className="h-dvh w-full flex justify-center items-center">
+          <div className="w-full bg-gray-500 flex flex-col justify-center h-full items-center">
+              <p className="text-black">This is the second page</p>
+              <button onClick={sendMessageToIframe} className="mb-4 px-4 py-2 bg-blue-600 text-white rounded">Send Message to Iframe</button>
+              <iframe ref={iframeRef} className="h-full w-full" src="https://first-mirror.vercel.app/" />
+          </div>
         </div>
-      </div>
-  )
+    )
 }
 
 export default Page;
