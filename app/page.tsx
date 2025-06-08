ec181a102187c195ffb3b0b96cdc0363f1beaaf9
Mohammad <damirchi159@gmail.com>
2025-06-08 15:37:14 -0400

    Add iframe

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 const Page = () => {
   return(
       <div className="h-dvh w-full flex justify-center items-center">
-        <div className="w-1/3 bg-gray-500 flex flex-col justify-center h-full items-center">
+        <div className="w-full bg-gray-500 flex flex-col justify-center h-full items-center">
             <p className="text-black">This is the second page</p>
-            <iframe className="h-full" src="https://first-mirror.vercel.app/" />
+            <iframe className="h-full w-full" src="https://first-mirror.vercel.app/" />
         </div>
       </div>
   )
